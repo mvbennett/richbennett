a092e06c6d8ed0393315a846e069536cd24c4f56
Mike Bennett <mvbennett9@gmail.com>
2023-01-16 14:05:24 -0500

    added title to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { Inter } from '@next/font/google'
+import Head from 'next/head'
 
 import Latest from './components/Latest'
 
@@ -7,6 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* <meta property="og:title" content="Rich Bennett Recording Producer" key="title" /> */}
+        <title>Rich Bennett Recording Producer</title>
+      </Head>
       <Latest />
     </>
   )
