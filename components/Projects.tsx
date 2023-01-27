886a380cc7a8f1f9f453aa5550a970520ca9441c
Michael Bennett <mvbennett9@gmail.com>
2023-01-27 02:30:12 -0500

    made a working nav for projects specifically

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 import underwaterSuper from '../public/images/underwater_supermarkets.jpg';
 import spacetronic from '../public/images/spacetronic_lunchbox.jpg';
@@ -11,59 +12,97 @@ import dibenedetto from '../public/images/dibenedetto.jpg';
 import soundParadise from '../public/images/sound_paradise.jpg';
 import outerSunset from '../public/images/outer_sunset.jpg';
 
+
 const Projects = () => {
   const projectsList = [
     {
       img: underwaterSuper,
       title: 'Music for Underwater Supermarkets',
-      link: 'https://open.spotify.com/album/7xJfTkpi8FhHiCwhmMJXMO?si=KCRuk_-JTkefBqS6CLLbOA'
+      link: 'https://open.spotify.com/album/7xJfTkpi8FhHiCwhmMJXMO?si=KCRuk_-JTkefBqS6CLLbOA',
+      artist: 'Roman Angelos'
     },
     {
       img: spacetronic,
       title: 'Spacetronic Lunchbox',
-      link: 'https://open.spotify.com/album/7l8qxzT9ABDGHBs5sN1Rt4?si=I1tohPSWTYyXCPBSrm-_PQ'
+      link: 'https://open.spotify.com/album/7l8qxzT9ABDGHBs5sN1Rt4?si=I1tohPSWTYyXCPBSrm-_PQ',
+      artist: 'Roman Angelos'
     },
     {
       img: italianSoda,
       title: 'Italian Soda',
-      link: 'https://open.spotify.com/album/27RtvlLiC4r4nUF4Hh6xvr?si=4CXZsfv0QTuaeaEfbz4jNQ'
+      link: 'https://open.spotify.com/album/27RtvlLiC4r4nUF4Hh6xvr?si=4CXZsfv0QTuaeaEfbz4jNQ',
+      artist: 'Roman Angelos'
     },
     {
       img: musicaAqui,
       title: 'Musica Aqui',
-      link: 'https://open.spotify.com/album/4UT873Eh7d1fiOZuFwrmmv?si=fk5XV8mrTTmQdbaDbhTwRQ'
+      link: 'https://open.spotify.com/album/4UT873Eh7d1fiOZuFwrmmv?si=fk5XV8mrTTmQdbaDbhTwRQ',
+      artist: 'Limoncello'
     },
     {
       img: pinkSand,
       title: "Pink Sand",
-      link: 'https://open.spotify.com/album/34va1Mi6xrlVYjbGw0eNkF?si=k492TVTaRkiju1wj0iog9Q'
+      link: 'https://open.spotify.com/album/34va1Mi6xrlVYjbGw0eNkF?si=k492TVTaRkiju1wj0iog9Q',
+      artist: 'Limoncello'
     },
     {
       img: dibenedetto,
       title: 'DiBenedetto',
-      link: 'https://open.spotify.com/track/1g5deMU74SVhCSmWl0QjsR?autoplay=true'
+      link: 'https://open.spotify.com/track/1g5deMU74SVhCSmWl0QjsR?autoplay=true',
+      artist: 'Rich Bennett'
     },
     {
       img: soundParadise,
       title: 'Transpacific Sound Paradise',
-      link: 'https://open.spotify.com/album/2A5YeZstkLJdjFqMyAggYH?at=10&uo=4'
+      link: 'https://open.spotify.com/album/2A5YeZstkLJdjFqMyAggYH?at=10&uo=4',
+      artist: 'Monocle'
     },
     {
       img: outerSunset,
       title: 'Outer Sunset',
-      link: 'https://open.spotify.com/album/6sf4AszxS6NIV23faj2FSp'
+      link: 'https://open.spotify.com/album/6sf4AszxS6NIV23faj2FSp',
+      artist: 'Monocle'
     }
   ];
-  const projects = projectsList.map((project) => {
+  const [projects, setProjects] = useState(projectsList)
+
+  let projectCards = projects.map((project) => {
     return <ProjectCard project={project} key={project.title} />;
   });
+
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+
+    setProjects(projectsList.filter((p) => {
+      return p.artist === e.target.value
+    }))
+  }
+
   return (
     <div>
       <Navbar page="secondary" />
       <div className="container">
         <h2>Projects</h2>
+        <ul className="project-nav">
+          <li>
+            <button onClick={handleClick} value="Roman Angelos">
+              Roman Angelos
+            </button>
+          </li>
+          <li>
+            <button onClick={handleClick} value="Limoncello">
+              Limoncello
+            </button>
+          </li>
+          <li>
+            <button onClick={handleClick} value="Monocle">
+              Monocle
+            </button>
+          </li>
+        </ul>
         <div className="projects">
-          {projects}
+          {projectCards}
         </div>
       </div>
     </div>
