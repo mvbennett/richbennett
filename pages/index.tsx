e9b97e5827ccf10cbee56c937da847901fa000f0
Michael Bennett <mvbennett9@gmail.com>
2023-03-07 03:22:28 -0500

    integrated contentful content

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,31 @@ import Latest from '../components/Latest'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-})
+export async function getStaticProps() {
 
-export default function Home() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  })
+
+  const res = await client.getEntries({ content_type: 'latestPost' });
+
+  return {
+    props: {
+      latestPosts: res.items
+    }
+  }
+}
+
+
+export default function Home({ latestPosts }: any) {
+  console.log(latestPosts[0].fields.picture.fields.file.url);
+  console.log(latestPosts[0].fields.title);
+  console.log(latestPosts[0].fields.postContent.content[0].content[0].value);
+  console.log(latestPosts[0].fields.postContent);
+
+
+
   return (
     <>
       <Head>
