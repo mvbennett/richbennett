6dc290aab4151aa79f545cb5341b4588d432670f
Michael Bennett <mvbennett9@gmail.com>
2023-03-07 01:53:54 -0500

    added env variable types to env.d.ts file

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,16 @@ import FormButton from '@/components/FormButton'
 import FormContainer from '@/components/FormContainer'
 import { Inter } from '@next/font/google'
 import Head from 'next/head'
+import { createClient } from 'contentful';
 
 import Latest from '../components/Latest'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+})
 
 export default function Home() {
   return (
