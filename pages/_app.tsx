93dae96b07d3e4c9a1e39650c5bfd4cb5c83f7cf
Michael Bennett <mvbennett9@gmail.com>
2023-03-17 12:59:12 -0400

    added app manifest and necessary icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return <Component {...pageProps} />
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=swap" />
         <meta name="description" content="Rich Bennett is a recording producer, mixer, and engineer as well as a composer and musician in his own right." />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#d1e1ea" />
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
       </Head>
       <body>
         <Main />
