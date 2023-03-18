a8b9add42f2b5fab24af90f1e20fb2f8617872fd
Michael Bennett <mvbennett9@gmail.com>
2023-03-18 15:25:13 -0400

    found a way to contain images without deprecated attribute

--- components/Post.tsx
+++ components/Post.tsx
@@ -13,7 +13,7 @@ const Post = ({post: { img, title, content } }:props) => {
   return(
     <div className="latest-post">
       <div className="post-image">
-        <Image className="latest-img" src={img} alt={title} priority={true} fill objectFit="contain" />
+        <Image className="latest-img" src={img} alt={title} priority={true} fill style={{ objectFit: 'contain'}} sizes="(max-width: 800px) 350px" />
       </div>
       <div className="latest-post-content">
         <h2>{title}</h2>
