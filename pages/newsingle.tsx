77ecdc77b628a4a22e6f0b44d92faa95327b2081
Mike Bennett <mvbennett9@gmail.com>
2023-07-25 00:32:42 -0400

    changed noreferrer issue

--- pages/newsingle.tsx
+++ pages/newsingle.tsx
@@ -11,11 +11,11 @@ export default function newsingle () {
       <iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1655402857&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
       </iframe>
       <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-        <a href="https://soundcloud.com/richbennett12" title="acmehallrich" target="_blank" style="color: #cccccc; text-decoration: none;">
+        <a href="https://soundcloud.com/richbennett12" title="acmehallrich" target="_blank" rel="noreferrer" style="color: #cccccc; text-decoration: none;">
           acmehallrich
         </a>
          ·
-        <a href="https://soundcloud.com/richbennett12/sets/new-roman-angelos-record" title="New Roman Angelos Record" target="_blank" style="color: #cccccc; text-decoration: none;">
+        <a href="https://soundcloud.com/richbennett12/sets/new-roman-angelos-record" title="New Roman Angelos Record" target="_blank" rel="noreferrer" style="color: #cccccc; text-decoration: none;">
           New Roman Angelos Record
         </a>
       </div>
