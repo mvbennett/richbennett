319f0c0124e8f5ca5fa76361cca98f74994e50ef
Mike Bennett <mvbennett9@gmail.com>
2023-07-25 00:43:16 -0400

    moved styles to module

--- pages/newsingle.tsx
+++ pages/newsingle.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from '../styles/newsingle.module.css'
 
 export default function newsingle () {
   return (
@@ -8,14 +9,14 @@ export default function newsingle () {
           New Roman Angelos Single
         </title>
       </Head>
-      <iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1655402857&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+      <iframe width="100%" height="600" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1655402857&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
       </iframe>
-      <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-        <a href="https://soundcloud.com/richbennett12" title="acmehallrich" target="_blank" rel="noreferrer" style="color: #cccccc; text-decoration: none;">
+      <div className={styles.div}>
+        <a href="https://soundcloud.com/richbennett12" title="acmehallrich" target="_blank" rel="noreferrer" className={styles.linkone}>
           acmehallrich
         </a>
          ·
-        <a href="https://soundcloud.com/richbennett12/sets/new-roman-angelos-record" title="New Roman Angelos Record" target="_blank" rel="noreferrer" style="color: #cccccc; text-decoration: none;">
+        <a href="https://soundcloud.com/richbennett12/sets/new-roman-angelos-record" title="New Roman Angelos Record" target="_blank" rel="noreferrer" className={styles.linktwo}>
           New Roman Angelos Record
         </a>
       </div>
