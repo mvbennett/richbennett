2d48936f83b7dcea8d4dac55bb55f68eedbecbe6
Mike Bennett <mvbennett9@gmail.com>
2024-04-17 12:44:29 -0400

    changed type of redirect fn

--- pages/api/spotify.ts
+++ pages/api/spotify.ts
@@ -1,11 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { redirect } from 'next/dist/server/api-utils'
+import { redirect } from 'next/navigation'
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'GET') {
-    redirect(res, 'url', 'https://open.spotify.com/artist/1RMBOy3qTuHdPWNJaopTbS?si=YlAPhHm8T3-Nf7laF-_Qdw')
+    res.status(200)
+    redirect('https://open.spotify.com/artist/1RMBOy3qTuHdPWNJaopTbS?si=YlAPhHm8T3-Nf7laF-_Qdw')
   }
 }
