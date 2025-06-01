fc29422993bb824fb2b94ef2230b73e847cf32c0
Mike Bennett <mvbennett9@gmail.com>
2025-06-01 12:27:23 -0400

    feat: added new single plyaer to newsingle path

--- pages/dj.tsx
+++ pages/dj.tsx
@@ -1,24 +1,12 @@
-import Head from "next/head";
-import styles from '../styles/components/SoundCloudPlayer.module.css'
-import SoundCloudPlayer from "@/components/SoundCloudPlayer";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function newsingle () {
-  return (
-    <>
-      <Head>
-        <title>
-          New Roman Angelos Single
-        </title>
-      </Head>
-      <div className={styles.main}>
-        <SoundCloudPlayer
-          title="Tropical Nites"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1886630076&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-          href="https://soundcloud.com/richbennett12/roman-dj-set-sample"
-        />
-      </div>
-    </>
-  )
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/')
+  }, [router])
 }
 
-/* src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1886630076&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" href="https://soundcloud.com/richbennett12/roman-dj-set-sample" */
+export default NotFound;
--- pages/newsingle.tsx
+++ pages/newsingle.tsx
@@ -1,12 +1,22 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Head from "next/head";
+import styles from '../styles/components/SoundCloudPlayer.module.css'
+import SoundCloudPlayer from "@/components/SoundCloudPlayer";
 
-const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/')
-  }, [router])
+export default function newsingle () {
+  return (
+    <>
+      <Head>
+        <title>
+          New Roman Angelos Single
+        </title>
+      </Head>
+      <div className={styles.main}>
+        <SoundCloudPlayer
+          title="City of Tomorrow"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2028860691&color=%238fb8be&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          href="https://soundcloud.com/romanangelosmusic/sets/city-of-tomorrow"
+        />
+      </div>
+    </>
+  )
 }
-
-export default NotFound;
